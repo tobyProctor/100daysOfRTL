4ef28889d712e94bab5fe4de93c36c72a144699f
tobyProctor <toby.proctor93@gmail.com>
2022-10-14 18:25:49 +0100

    Adding finished day 4

--- day_4/alu.sv
+++ day_4/alu.sv
@@ -1,25 +1,47 @@
-module edge_detector (
-  input   wire clk,
-  input   wire reset,
-
+module alu (
   input   wire [7:0] data_a_i,
   input   wire [7:0] data_b_i,
 
-  input   wire [3:0] op_i,
+  input   wire [2:0] op_i,
 
   output  reg  [7:0] alu_o
 );
 
-    logic data_delayed;
+    /*
+    ALU Operations:
+    3'b000	ADD	-
+    3'b001	SUB	-
+    3'b010	SLL	- Vector a should left shift using bits 2:0 of vector b
+    3'b011	LSR - Vector a should right shift using bits 2:0 of vector b
+    3'b100	AND	-
+    3'b101	OR	-
+    3'b110	XOR	-
+    3'b111	EQL	- Output should be 1 when equal otherwise 0
+    */
+    localparam  ADD = 3'b000;
+    localparam  SUB = 3'b001;
+    localparam  SLL = 3'b010;
+    localparam  LSR = 3'b011;
+    localparam  AND = 3'b100;
+    localparam  OR  = 3'b101;
+    localparam  XOR = 3'b110;
+    localparam  EQL = 3'b111;
 
-    always_ff @(posedge clk or posedge reset) begin : delay_one_cycle
-        if (reset) begin
-            data_delayed <= 'b0;
-        end else begin
-            data_delayed <= data_i;
-        end
+    // always_comb and case statements not supported in iverilog
+    always @(op_i or data_a_i or data_b_i) begin : ALU_logic
+      case (op_i)
+         ADD : alu_o = data_a_i + data_b_i;
+         SUB : alu_o = data_a_i - data_b_i;
+         SLL : alu_o = data_a_i << data_b_i[2:0];
+         LSR : alu_o = data_a_i >> data_b_i[2:0];
+         AND : alu_o = data_a_i & data_b_i;
+         OR  : alu_o = data_a_i | data_b_i;
+         XOR : alu_o = data_a_i ^ data_b_i;
+         EQL : alu_o = {7'h0, data_a_i == data_b_i};
+         default : begin
+          alu_o = 8'b0;
+         end
+      endcase
     end
-
-    assign rising_edge_o = ~data_delayed & data_i;
 
 endmodule
--- day_4/alu_tb.sv
+++ day_4/alu_tb.sv
@@ -1,27 +1,22 @@
 module alu_tb ();
 
-    logic clk;
-    logic reset;
+    logic [7:0] data_a_i;
+    logic [7:0] data_b_i;
 
-    logic [7:0] data_a_i,
-    logic [7:0] data_b_i,
+    logic [2:0] op_i;
 
-    logic [3:0] op_i,
-
-    logic [7:0] alu_o
-
-    // Generate clk
-    always begin
-        clk = 1'b1;
-        #1;
-        clk = 1'b0;
-        #1;
-    end
+    logic [7:0] alu_o;
 
     alu alu1 (.*);
     
     initial begin
-
+        for (int i = 0; i < 10; i++) begin
+            data_a_i = $urandom_range (0, 8'hFF);
+            data_b_i = $urandom_range (0, 8'hFF);
+            op_i = $urandom_range (0, 3'b111);
+            #1;
+            $display("data_a_i=%d data_b_i=%d op_i=%d, alu_o=%d", data_a_i, data_b_i, op_i, alu_o);
+        end
         $finish;
     end
 
